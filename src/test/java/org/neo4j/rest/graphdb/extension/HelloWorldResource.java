9de8be727e257c8c73b547036b95fdbe26a7759a
Michael Hunger <github@jexp.de>
2011-10-21 00:59:35 +0200

    moved hw-extension to separate package

--- src/test/java/org/neo4j/rest/graphdb/extension/HelloWorldResource.java
+++ src/test/java/org/neo4j/rest/graphdb/extension/HelloWorldResource.java
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.neo4j.rest.graphdb;
+package org.neo4j.rest.graphdb.extension;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 
